3d2fc39eeb127f1aaf3decf2834097f58d6993b9
zyx20100911 <65153966+zyx20100911@users.noreply.github.com>
2021-12-12 07:49:43 +0800

    创建项目，完成静态头尾部分

---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+//挂载插件
+Vue.use(VueRouter);
+//引入组件
+import home from "../view/home";
+import login from "../view/login";
+import register from "../view/register";
+import search from "../view/search";
+
+//避免重复跳转报错 重写vueRouter原型方法
+const VueRouterPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (to) {
+    return VueRouterPush.call(this, to).catch(err => err)
+}
+//配置路由
+export default new VueRouter({
+    //路由配置
+    routes:[
+        {
+            path: "/home",
+            component: home,
+            name:"home",
+            meta:{
+                isShowFooter:true
+            }
+        },
+        {
+            path: "/login",
+            component: login,
+            meta:{
+                isShowFooter:false
+            }
+        },
+        {
+            path: "/register",
+            component: register,
+            meta:{
+                isShowFooter:false
+            }
+        },
+        {
+            path: "/search/:kewords",
+            component: search,
+            name:"search",
+            meta:{
+                isShowFooter:true
+            }
+        },
+        {
+            path: "/",
+            redirect:"/home"
+        }
+    ]
+})
---
+++ src/view/search.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h2>{{$route.params}}</h2>
+  <h2>{{$route.query}}</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: "search"
+}
+</script>
+
+<style scoped>
+
+</style>
