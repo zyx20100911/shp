ddcea923a347934c8ef7e58bf84d53042a63a87f
zyx20100911 <65153966+zyx20100911@users.noreply.github.com>
2021-12-13 05:17:44 +0800

    完成首页静态页面

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 //在入口引入路由
 import router from '@/router'
+//引入自定义组件
+import TypeNav from "./view/Home/TypeNav";
+Vue.component('TypeNav',TypeNav)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/view/home.vue
+++ src/view/home.vue
@@ -1,12 +1,33 @@
 <template>
 <div>
-  home
+  <TypeNav/>
+  <listContainer/>
+  <todayRecommend/>
+  <rank/>
+  <like/>
+  <floor/>
+  <floor/>
+  <brand/>
 </div>
 </template>
 
 <script>
+import listContainer from "./Home/listContainer";
+import todayRecommend from "./Home/todayRecommend";
+import rank from "./Home/rank";
+import like from "./Home/like";
+import floor from "./Home/floor";
+import brand from "./Home/brand";
 export default {
-  name: "home"
+  name: "home",
+  components:{
+    listContainer,
+    todayRecommend,
+    rank,
+    like,
+    floor,
+    brand
+  }
 }
 </script>
 
